76e191cf961b40bba087083f831e6a03c80dc3f6
meshramsaroj <meshramsaroj>
2020-06-09 14:26:25 +0530

    Updated config

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 import { environment} from '../environments/environment';
 
 
-export const baseUrl = environment.production ? 'https://api.shoppingcart.com' : 'http://localhost:3000';
+export const baseUrl = environment.production ? 'https://my-json-server.typicode.com' : 'https://my-json-server.typicode.com/meshramsaroj/mockJson';
 export const productUrl = baseUrl + '/products';
 export const cartUrl = baseUrl + '/cart';
 export const wishlistUrl = baseUrl + '/wishlist';
